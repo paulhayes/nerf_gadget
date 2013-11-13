b72cf5da094951f66f6c128595d0b523e3951bf6
Paul Hayes <paul.hayes@entropedia.co.uk>
2013-11-13 19:04:20 +0000

    First fully featured working prototype

--- NerfGadget.ino
+++ NerfGadget.ino
@@ -1,24 +1,45 @@
 #include <LedControl.h>
 #include<stdlib.h>
 
+const int MODE_MAGAZINE = 0;
+const int MODE_SPEEDOMETER = 1;
+
 const int irled = 12;
 const int ird = 2;
 const int led = 13;
+
+const int modeButton = 2;
+const int actionButton = 3;
+const int resetButton = 4;
+
 const int maxDuration = (0ul)-1;
 const int THRESHOLD = 900;
 double nerfDartLength = 0.071;
  
- 
+
 unsigned long startTime;
 unsigned long endTime;
 unsigned long lastIrChange;
 boolean irOn;
- 
+
+boolean modeButtonLastState;
+boolean actionButtonLastState;
+boolean resetButtonLastState;
+
+int mode = 0;
+
+const int numMagazines = 4;
+int magazine = 0;
+int magazines[4] = {6,18,25,35};
+
 LedControl lc1=LedControl(10,11,12,2);
  
 void setup(){
   pinMode(irled,OUTPUT);
   pinMode(led,OUTPUT);
+ 
+  pinMode( modeButton, INPUT );
+  pinMode( actionButton, INPUT );
  
   irOn = false;
  
@@ -39,56 +60,129 @@ void setup(){
 }
  
 void loop(){
-  while( true ){
  
   if( analogRead(ird ) > THRESHOLD ){
      
-      startTime = micros();
-     
-      digitalWrite(led,HIGH);
-     
-      while( analogRead(ird ) > THRESHOLD ){
-          //delayMicroseconds(8);
-          
-          
-      }
-     
-      digitalWrite(led,LOW);
- 
-      endTime = micros();
-     
-      unsigned long duration = 0;
-      if( endTime < startTime ) {
-        duration = ( maxDuration - startTime ) + endTime;
-      }
-      else duration = endTime - startTime;
-     
-      double speed = nerfDartLength * 1000000.0 / duration;
-      int speedRound = speed + 0.5;
-      Serial.println( duration );
-      Serial.print( "speed: " );
-      Serial.println( speed );
-      Serial.print( "speed rounded: " );
-      Serial.println( speedRound );
-      
-      char buffer[4];
-      unsigned char minStringWidthIncDecimalPoint = 4;
-      char numVarsAfterDecimal = 2;
-      dtostrf( speed, minStringWidthIncDecimalPoint, numVarsAfterDecimal, buffer );
-      
-      Serial.print( "speed: " );
-      Serial.println( buffer );
-      int j=0;
-      for(int i=0;i<4;i++){
-        if( buffer[i] == '.' || buffer[i] == '0' ) continue;
-        byte number = buffer[i] - 48;
-        boolean isDotNext = i<2 && buffer[i+1] == '.';
-        lc1.setDigit(0,j,number,isDotNext);
-        j++;
-      }
+    startTime = micros();  
+    digitalWrite(led,HIGH);
+    while( analogRead(ird ) > THRESHOLD ){
+        //delayMicroseconds(8);          
+    }
+    digitalWrite(led,LOW);
+    endTime = micros();
+    
+    /* ir sensor tripped */
+    switch(mode){
+      case MODE_MAGAZINE:
+        magazine--;
+        MagazineMode();
+        break;
+      case MODE_SPEEDOMETER:
+        SpeedometerMode();
+        break;
+    }
   }
- 
+  
+  CheckButtons();
+}
+
+inline void CheckButtons(){
+  boolean modeButtonState = digitalRead( modeButton ) == HIGH;
+  boolean actionButtonState = digitalRead( actionButton ) == HIGH;  
+  boolean resetButtonState = digitalRead( resetButton ) == HIGH;
+  
+  if( modeButtonState != modeButtonLastState && !modeButtonState ){
+    mode = ( mode + 1 ) % 2;
+    displayInt( 0 );
+    Serial.print("mode pressed ");
+    Serial.println( mode );
   }
+
+  if( actionButtonState != actionButtonLastState && !actionButtonState ){
+    ActionPressed();
+    Serial.println("action pressed");
+  }
+
+  if( resetButtonState != resetButtonLastState && !resetButtonState ){
+    ResetPressed();
+    Serial.println("reset pressed");
+
+  }
+  
+  modeButtonLastState = modeButtonState;
+  actionButtonLastState = actionButtonState;
+  resetButtonLastState = resetButtonState;
+}
+
+inline void ActionPressed(){
+  if( mode == MODE_MAGAZINE ){
+    magazine--;
+    MagazineMode();
+  }
+  else {
+  }
+}
+
+inline void ResetPressed(){
+  if( mode == MODE_MAGAZINE ){
+    int i=0;
+    while( magazines[i] <= magazine && i<numMagazines ) i++;
+    if( i==numMagazines ) i=0;
+    Serial.println( i );
+    magazine = magazines[i];
+    MagazineMode();
+  }
+  else {
+    
+  }
+}
+
+void SpeedometerMode(){
+  unsigned long duration = 0;
+  if( endTime < startTime ) {
+    duration = ( maxDuration - startTime ) + endTime;
+  }
+  else duration = endTime - startTime;
+   
+  double speed = nerfDartLength * 1000000.0 / duration;
+  
+  if( Serial ){
+    Serial.println( duration );
+    Serial.print( "speed: " );
+    Serial.println( speed );      
+  }
+  
+  displayDouble( speed );
+}
+
+inline void MagazineMode(){
+  if( magazine < 0 ) magazine = 0;
+  displayInt( magazine );
+}
+
+void displayInt(int value){
+  byte tens = ( value / 10 ) % 10;
+  byte ones = value % 10;
+  
+  lc1.setDigit(0,0,tens,false);
+  lc1.setDigit(0,1,ones,false);
+
+}
+
+void displayDouble(double value){
+  char buffer[4];
+  unsigned char minStringWidthIncDecimalPoint = 4;
+  char numVarsAfterDecimal = 2;
+  dtostrf( value, minStringWidthIncDecimalPoint, numVarsAfterDecimal, buffer );
+
+  int j=0;
+  for(int i=0;i<4;i++){
+    if( buffer[i] == '.' || buffer[i] == '0' ) continue;
+    byte number = buffer[i] - 48;
+    boolean isDotNext = i<2 && buffer[i+1] == '.';
+    lc1.setDigit(0,j,number,isDotNext);
+    j++;
+  }  
 }
 
 
